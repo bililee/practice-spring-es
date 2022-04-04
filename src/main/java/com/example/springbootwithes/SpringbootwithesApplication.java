2dbaf70b8f01de25528a5db2dbf0c7ef4d53bdb0
lee <airsunny@icloud.com>
2022-04-04 21:49:34 +0800

    initialize demo

---
+++ src/main/java/com/example/springbootwithes/SpringbootwithesApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootwithes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootwithesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootwithesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootwithes/SpringbootwithesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootwithes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootwithesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
